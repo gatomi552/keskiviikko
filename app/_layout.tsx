1d71924022fbd64f600d76d582bd968c37a6e04d
matiasmaldonado526 <matiasmaldonado526@gmail.com>
2025-11-24 19:18:04 +0000

    ✨feat(settings)se crea un nuevo archivo settings -Se coloca el codigo en _layout para unificar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,16 @@
-import { Stack } from "expo-router";
+import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabLayout() {
+  return (
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <Label>Home</Label>
+        <Icon sf="house.fill" drawable="custom_android_drawable" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="settings">
+        <Icon sf="gear" drawable="custom_settings_drawable" />
+        <Label>Settings</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
 }
