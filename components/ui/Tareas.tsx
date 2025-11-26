efb8ab03e9f0c90aefadd153b5906bbe099e186f
matiasmaldonado526 <matiasmaldonado526@gmail.com>
2025-11-26 15:26:48 +0000

    ✨feat(db)Se agrega un servidor ni idea como funciona -con sus respectivas dependencias

---
+++ components/ui/Tareas.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Tareas() {
+
+  return (
+    <View className="flex-1 justify-center items-center bg-emerald-900/25 h-screen text-center">
+      <Text>
+      </Text>
+    </View>
+  );
+}
