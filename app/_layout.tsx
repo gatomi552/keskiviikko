ec7cb444f620e35020873fd1f8bf872072dea4e9
matiasmaldonado526 <matiasmaldonado526@gmail.com>
2025-12-03 16:02:28 +0000

    🎨Style(_layout) colocacion de iconos no funcionaxd

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,16 +3,29 @@ import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs';
 export default function TabLayout() {
   return (
     <NativeTabs>
+
       <NativeTabs.Trigger name="index">
         <Label>Home</Label>
-        <Icon sf="house.fill" drawable="custom_android_drawable" />
+        <Icon 
+          // SF Symbol (iOS)
+          sf="house.fill"  
+        />
       </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="profile">{/*Se necesita el nombre de la carpeta xd*/}
+
+      <NativeTabs.Trigger name="profile">
         <Label>Perfiles</Label>
+        <Icon
+          sf="person.crop.circle.fill"
+        />
       </NativeTabs.Trigger>
+
       <NativeTabs.Trigger name="settings">
       <Label hidden />
+        <Icon
+          sf="gearshape.fill"
+        />
       </NativeTabs.Trigger>
+
     </NativeTabs>
   );
 }
--- app/profile.tsx
+++ app/profile.tsx
@@ -7,20 +7,25 @@ import "@/global.css";
 export default function Tab() {
   return (
     <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      className="flex-1">
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 80 : 0}
+      className="flex-1"
+    >
       <ScrollView
-        contentContainerStyle={{ flexGrow: 1 }}
-        keyboardShouldPersistTaps="handled">
-        <View className="flex-1 justify-center items-center bg-rose-400/15 h-screen text-center px-4">
+        contentContainerStyle={{ flexGrow: 1, justifyContent: "center" }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <View className="flex-1 justify-center items-center bg-rose-400/15 px-4 py-8">
           <Text className="text-xl font-bold text-gray-900 mb-2">
             Escribe tu nombre
           </Text>
           <Textname />
+
           <Text className="text-xl font-bold text-gray-900 mb-2">
             Escribe tu email
           </Text>
           <TextField />
+
           <Text className="text-xl font-bold text-gray-900 mb-2">
             Escribe tu contraseña
           </Text>
